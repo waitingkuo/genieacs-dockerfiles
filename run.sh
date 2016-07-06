42934bd491effe71c69c1a3c18b2f9fb5c05eef1
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2016-07-06 18:41:22 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 docker run -d --net=host redis
 docker run -d --net=host mongo
 docker run -d --net=host linkerrepository/genieacs-cwmp
-docker run -d --net=host linkerrepository/genieacs genieacs-nbi
-docker run -d --net=host linkerrepository/genieacs genieacs-fs
+docker run -d --net=host linkerrepository/genieacs-nbi
+docker run -d --net=host linkerrepository/genieacs-fs
 docker run -d --net=host linkerrepository/genieacs-gui
