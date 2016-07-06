69283c29030bb5d649adda9d26a32a1a19d9f13b
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2016-07-06 17:59:22 +0800

    init

---
+++ build.sh
@@ -0,0 +1,5 @@
+docker build -t linkerrepository/genieacs      genieacs
+docker build -t linkerrepository/genieacs-cwmp genieacs-cwmp
+docker build -t linkerrepository/genieacs-nbi  genieacs-nbi
+docker build -t linkerrepository/genieacs-fs   genieacs-fs
+docker build -t linkerrepository/genieacs-gui  genieacs-gui
